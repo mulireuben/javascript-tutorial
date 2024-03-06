518f6ebb71794827820f500fcc32fb4251aa91ac
muli reuben <mulireuben57@gmail.com>
2024-03-06 10:25:13 +0300

    added some items on dom manipulation

---
+++ index.scrimba/index.js
@@ -0,0 +1,214 @@
+// //document.getElementById("count-el").innerText = 5
+
+
+// let count=5
+// console.log(count)
+
+// // create a variable my age and assign the age and use console.log to check the age
+
+// let myAge=25
+
+// console.log(myAge)
+
+
+
+// //how to do operators in javascript
+// //i want to calculate the age of my dog whereby i human year is equivalent to 7 dog years
+// // lets go
+
+
+// let mage=35
+// let humandogratio =7
+
+// let mydogage= mage * humandogratio
+// console.log(mydogage)
+
+
+// // Create a variable, bonusPoints. Initialize it as 50. Increase it to 100.
+// // Decrease it down to 25, and then finally increase it to 70
+// // Console.log the value after each step
+
+// let bonuspoint=50
+// console.log(bonuspoint)
+
+// bonuspoint=bonuspoint+50
+// console.log(bonuspoint)
+
+// bonuspoint=bonuspoint-75
+// console.log(bonuspoint)
+
+// bonuspoint=bonuspoint + 45
+// console.log(bonuspoint)
+
+// //on click event
+// // intialize the count as 0
+// // listen for clicks on the increment button
+// // increment the count variable when the button is clicked
+// // change the count-el in the HTML to reflect the new count
+
+
+
+// function increment() {
+//     console.log("The button was clicked")
+
+// }
+
+
+
+
+
+
+// //using function to write less code,i could have repeated the console.log(number) several 
+// //times but i decide to use function countdown
+// function countdown(){
+
+//     console.log(5)
+//     console.log(4)
+//     console.log(3)
+//     console.log(2)
+//     console.log(1)
+// }
+
+// countdown()
+// countdown()
+// countdown()
+
+
+// //create a function that callsnumber 45 in the console;
+
+
+// function call(){
+//     console.log(42)
+// }
+
+// call()
+
+// //use a function to do the sum;
+
+// let a=40
+// let b=49
+// let c=59
+
+// function addition(){
+
+//     let addition=a+b+c
+//     console.log(addition)
+// }
+
+// addition()
+
+
+
+// //am going to write a code that increments a laptime
+
+// let lapcomplete=0
+
+// //create a variable that increments the laps completed by one
+// //and i will run it three times
+// function lapincrement(){
+
+// lapcomplete=lapcomplete+1
+// }
+
+// lapincrement()
+// lapincrement()
+// lapincrement()
+
+// console.log(lapcomplete)
+
+
+// //array functions
+// //i use array of patients name
+
+// //array functions
+// //i use array of patients name
+
+// const patients=["mutaga","jeremy","emilly","stephen","michael"];
+//  //array mapping-mapping for each name according to the function given to the array
+// //function to add disease to each patientname ,,,add disease
+// const newarr = patients.map(
+//     function(name){
+//       return  name += " polio"
+//     }
+// )
+// console.log(newarr
+//     )
+// //map function to add disease to each patient
+
+
+
+
+// // function addDisease(name,disease){
+// //     return '{name} ({disease})';
+
+// // }
+// // //map function to add disease to each patient
+// // const patientsWithdDisease=patients.map(patients=>addDisease(disease,"flu"));
+
+
+
+// //  console.log("patients with disease:",patientsWithdDisease);
+
+// let a=10
+// let b=20
+// let c=40
+
+
+// function addition (){
+//   let addition=a+b+c
+// }
+
+// console.log(addition)
+
+// addition()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// console.log(helo muli)
+
+// let count=0
+
+// function increment(){
+//     count=count + 1
+//     console.log(count)
+// }
+
+//DOM MANIPULATION
+
+const heading=document.getElementById('main heading');
+console.log(heading)
+
+const listItem=document.getElementsByClassName("list items")
+console.log(listItem)
